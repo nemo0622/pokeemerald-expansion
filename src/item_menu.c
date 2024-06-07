8b38d6a105452b512e577235a4235a739e37e10d
Jack Wellman <jwwellman1@gmail.com>
2024-06-06 23:15:42 -0400

    Party and Bag menu UI updates and font updates

--- src/item_menu.c
+++ src/item_menu.c
@@ -1423,11 +1423,11 @@ static void DrawItemListBgRow(u8 y)
 
 static void DrawPocketIndicatorSquare(u8 x, bool8 isCurrentPocket)
 {
-    if (!isCurrentPocket)
-        FillBgTilemapBufferRect_Palette0(2, 0x1017, x + 5, 3, 1, 1);
-    else
-        FillBgTilemapBufferRect_Palette0(2, 0x102B, x + 5, 3, 1, 1);
-    ScheduleBgCopyTilemapToVram(2);
+    // if (!isCurrentPocket)
+    //     FillBgTilemapBufferRect_Palette0(2, 0x1017, x + 5, 3, 1, 1);
+    // else
+    //     FillBgTilemapBufferRect_Palette0(2, 0x102B, x + 5, 3, 1, 1);
+    // ScheduleBgCopyTilemapToVram(2);
 }
 
 static bool8 CanSwapItems(void)
